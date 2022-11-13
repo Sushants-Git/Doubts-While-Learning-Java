f968853984020f26e604d6671d3d0bdbc9839569
Sushant Mishra <100516354+Sushants-Git@users.noreply.github.com>
2022-11-13 12:14:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-Rounding a double to 5 decimal places
-You can round to the fifth decimal place by making it the first decimal place by multiplying your number. Then do normal rounding, and make it the fifth decimal place again.
+// Rounding a double to 5 decimal places
+// You can round to the fifth decimal place by making it the first decimal place by multiplying your number. Then do normal rounding, and make it the fifth decimal place again.
 
-Let's say the value to round is a double named x:
+// Let's say the value to round is a double named x:
 
 double factor = 1e5; // = 1 * 10^5 = 100000.
 double result = Math.round(x * factor) / factor;
 
-If you want to round to 6 decimal places, let factor be 1e6, and so on.
+// If you want to round to 6 decimal places, let factor be 1e6, and so on.
