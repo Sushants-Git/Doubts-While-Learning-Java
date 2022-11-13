7cf097fd7c2ea6c6f241eaecb041199c176dee9e
Sushant Mishra <100516354+Sushants-Git@users.noreply.github.com>
2022-11-13 12:15:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+{
 // Rounding a double to 5 decimal places
 // You can round to the fifth decimal place by making it the first decimal place by multiplying your number. Then do normal rounding, and make it the fifth decimal place again.
 
@@ -7,3 +8,4 @@ double factor = 1e5; // = 1 * 10^5 = 100000.
 double result = Math.round(x * factor) / factor;
 
 // If you want to round to 6 decimal places, let factor be 1e6, and so on.
+}
