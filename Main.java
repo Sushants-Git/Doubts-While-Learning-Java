3b5d34d3da32e92ee02372b7f7c99f3c6b43ca8c
Sushant Mishra <100516354+Sushants-Git@users.noreply.github.com>
2022-11-15 21:44:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,3 +9,34 @@ double result = Math.round(x * factor) / factor;
 
 // If you want to round to 6 decimal places, let factor be 1e6, and so on.
 }
+
+
+{
+//Static methods in Java are inherited, but can not be overridden.
+  
+public class Writer {
+    public static void write() {
+        System.out.println("Writing");
+    }
+}
+
+public class Author extends Writer {
+    public static void write() {
+        System.out.println("Writing book");
+    }
+}
+
+public class Programmer extends Writer {
+
+    public static void write() {
+        System.out.println("Writing code");
+    }
+
+    public static void main(String[] args) {
+        Writer thirdWriter = null;
+        thirdWriter.write();
+    }
+}
+  
+//-----> Output :- Writing
+}
