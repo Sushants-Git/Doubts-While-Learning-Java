fdc2fc7b7796681f1ca01fc0e9abd77581162580
Sushant Mishra <100516354+Sushants-Git@users.noreply.github.com>
2022-11-16 07:04:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-{
 // Rounding a double to 5 decimal places
 // You can round to the fifth decimal place by making it the first decimal place by multiplying your number. Then do normal rounding, and make it the fifth decimal place again.
 
@@ -8,10 +7,9 @@ double factor = 1e5; // = 1 * 10^5 = 100000.
 double result = Math.round(x * factor) / factor;
 
 // If you want to round to 6 decimal places, let factor be 1e6, and so on.
-}
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-{
 //Static methods in Java are inherited, but cannot be overridden.
   
 public class Writer {
@@ -39,4 +37,20 @@ public class Programmer extends Writer {
 }
   
 //-----> Output :- Writing
-}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public static int GCD(int a, int b){
+        // Here it does not matter if a>b or b>a.
+        // Let's say our desire was that 'a' was the greater term.
+        // if 'a' is the greater term we are fine with it.
+        if (a % b == 0)
+         return b;
+         else
+         return GCD(b, a % b);
+         // Here even if 'a' is not the greater term.
+         // The greater term 'b' will become 'a' and since a<b => a%b = a.
+         // so it will become greater,smaller irrespective of the input
+    }
+  // Complexity log(a+b) (https://codility.com/media/train/10-Gcd.pdf)
+------------------------------------------------------------------------------------------------------------------------------------------------------------
