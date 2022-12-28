c3fe7a0bbca6ec7ebce93046c4925a5c43a90dbb
Sushant Mishra <100516354+Sushants-Git@users.noreply.github.com>
2022-12-28 19:13:49 +0530

    Update Doubts.java

--- Doubts.java
+++ Doubts.java
@@ -113,3 +113,8 @@ static int Min(int[] arr, int index) {
 // constructor for any class without constructors. This default constructor will call the no-argument constructor of the superclass. In this situation, the 
 // compiler will complain if the superclass doesn't have a no-argument constructor so you must verify that it does. If your class has no explicit superclass, then it 
 // has an implicit superclass of Object, which does have a no-argument constructor.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Nested classes are divided into two categories: non-static and static. Non-static nested classes are called inner classes. Nested classes that are declared static
+// are called static nested classes. 
